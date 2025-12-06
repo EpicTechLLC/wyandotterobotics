23ac66bfc4e6a8d2f2a9a01dbfadc6111f0a905b
Oscar Campos-Soto <ocampossoto1@gmail.com>
2025-12-06 16:10:47 -0600

    Package updates

--- astro.config.ts
+++ astro.config.ts
@@ -25,9 +25,6 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'server',
-  experimental: {
-    contentCollectionCache: true, // Enable Incremental Content Caching
-  },
   build: {
     concurrency: 8,
   },
